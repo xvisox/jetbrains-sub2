dd2d8dc27ba0f1ec5ab3edd2a9ed1bd4dce20a7a
Hubert Michalski <43937184+xvisox@users.noreply.github.com>
2023-12-22 16:02:04 +0100

    compile performance (#1)

--- src/main/kotlin/ui/common/Constants.kt
+++ src/main/kotlin/ui/common/Constants.kt
@@ -6,6 +6,8 @@ class Constants {
     companion object {
         // FIXME: move to properties file
         const val EXAMPLE_FILE_PATH = "./src/main/resources/example.main.kts"
+        const val KOTLINC_SCRIPT_COMMAND = "kotlinc -script "
+        const val BATCH_UPDATE_INTERVAL = 500L
         val codeEditorBackgroundColor = Color(0xFF8C9A9E)
         val codeEditorTextColor = Color(0xFF000000)
         val navBarBackgroundColor = Color(0xFF747578)
--- src/main/kotlin/ui/editor/CodeEditor.kt
+++ src/main/kotlin/ui/editor/CodeEditor.kt
@@ -1,8 +1,11 @@
 package ui.editor
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import ui.common.Constants
 import java.io.BufferedReader
 import java.io.File
-import java.io.InputStream
 import java.io.InputStreamReader
 
 class CodeEditor {
@@ -16,24 +19,42 @@ class CodeEditor {
         }
     }
 
-    fun runKotlinScript(scriptFilePath: String): ProcessBuilder {
-        val command = "kotlinc -script $scriptFilePath"
+    suspend fun runKotlinScript(lines: MutableList<String>) = withContext(Dispatchers.IO) {
+        val process = getKotlinScriptRunnerProcessBuilder(Constants.EXAMPLE_FILE_PATH).start()
+        val reader = getProcessOutputReader(process)
+        val buffer = mutableListOf<String>()
+        val mutex = Mutex(locked = false)
+
+        launch {
+            lines.clear()
+            while (isActive) {
+                delay(Constants.BATCH_UPDATE_INTERVAL)
+                mutex.withLock {
+                    withContext(Dispatchers.Default) {
+                        lines.addAll(buffer)
+                    }
+                    buffer.clear()
+                }
+            }
+        }
+
+        while (true) {
+            val line = reader.readLine() ?: break
+            mutex.withLock { buffer.add(line) }
+        }
+
+        process.waitFor()
+    }
+
+    private fun getProcessOutputReader(process: Process) = BufferedReader(InputStreamReader(process.inputStream))
+
+    private fun getKotlinScriptRunnerProcessBuilder(scriptFilePath: String): ProcessBuilder {
+        val command = "${Constants.KOTLINC_SCRIPT_COMMAND}$scriptFilePath"
 
         val processBuilder = ProcessBuilder()
         processBuilder.command("bash", "-c", command)
         processBuilder.redirectErrorStream(true)
 
         return processBuilder
-//            // Read the output of the command
-//            val reader = BufferedReader(InputStreamReader(process.inputStream))
-//            var line: String?
-//
-//            while (reader.readLine().also { line = it } != null) {
-//                println(line)
-//            }
-//
-//            // Wait for the process to finish
-//            return process.waitFor()
-
     }
 }
