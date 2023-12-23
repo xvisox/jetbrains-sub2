c82d8ddd2f743bfaaab929d8fb881c62e8b8c866
xvisox <hubertmirek@gmail.com>
2023-12-23 13:05:06 +0100

    add multiple times script runner

--- src/main/kotlin/ui/NavBarView.kt
+++ src/main/kotlin/ui/NavBarView.kt
@@ -1,14 +1,14 @@
 package ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
@@ -20,31 +20,54 @@ import ui.editor.CodeEditor
 
 @Composable
 fun NavBarView(codeEditor: CodeEditor, lines: MutableList<String>, codeText: String) =
-    Row(ReusableModifiers.navBarModifier) {
+    Column {
         var loading by remember { mutableStateOf(false) }
+        var currentProgress by remember { mutableStateOf(0f) }
         var result by remember { mutableStateOf(-1) }
+        var batchSize by remember { mutableStateOf(1) }
 
-        Button(
-            onClick = {
-                loading = true
-                CoroutineScope(Dispatchers.Default).launch {
-                    codeEditor.runKotlinScript(lines, codeText) { result = it }
-                    loading = false
-                }
-            },
-            colors = ButtonDefaults.buttonColors(backgroundColor = Constants.runButtonColor),
-            enabled = !loading
-        ) {
-            Text(Constants.RUN_CODE, fontSize = 20.sp)
+        Row(ReusableModifiers.navBarModifier) {
+            Button(
+                onClick = {
+                    loading = true
+                    CoroutineScope(Dispatchers.Default).launch {
+                        for (i in 1..batchSize) {
+                            codeEditor.runKotlinScript(lines, codeText) { result = it }
+                            currentProgress = (i + 1).toFloat() / batchSize
+                        }
+                        loading = false; currentProgress = 0f
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Constants.runButtonColor),
+                enabled = !loading
+            ) {
+                Text(Constants.RUN_CODE, fontSize = 31.sp, color = Constants.textColor)
+            }
+
+            if (!loading) {
+                TextField(
+                    value = batchSize.toString(),
+                    onValueChange = { batchSize = it.toIntOrNull() ?: 1 },
+                    label = { Text(Constants.BATCH_SIZE, color = Constants.textColor) },
+                    modifier = Modifier.align(Alignment.CenterVertically).width(150.dp).padding(start = 20.dp),
+                    textStyle = TextStyle.Default.copy(color = Constants.textColor, fontSize = 20.sp)
+                )
+            } else CircularProgressIndicator(ReusableModifiers.circularProgressModifier)
+
+            if (result != -1) {
+                Text(
+                    "${Constants.EXIT_CODE}$result",
+                    fontSize = 32.sp,
+                    color = if (result == 0) Constants.textColor else Constants.failTextColor,
+                    modifier = Modifier.align(Alignment.CenterVertically).padding(start = 20.dp)
+                )
+            }
         }
-
-        if (loading) CircularProgressIndicator(ReusableModifiers.circularProgressModifier)
-        else if (result != -1) {
-            Text(
-                "${Constants.EXIT_CODE}$result",
-                fontSize = 32.sp,
-                color = if (result == 0) Constants.textColor else Constants.failTextColor,
-                modifier = Modifier.align(Alignment.CenterVertically).padding(start = 20.dp)
+        if (loading && batchSize > 1) {
+            LinearProgressIndicator(
+                currentProgress,
+                modifier = ReusableModifiers.linearProgressModifier
             )
         }
     }
+
--- src/main/kotlin/ui/common/Constants.kt
+++ src/main/kotlin/ui/common/Constants.kt
@@ -11,13 +11,14 @@ class Constants {
         const val TEMP_FILE_PREFIX = "temp"
         const val TEMP_FILE_SUFFIX = ".main.kts"
 
+        const val BATCH_SIZE = "Batch run size"
         const val RUN_CODE = "Run Code"
         const val EXIT_CODE = "Exit code: "
 
         val textColor = Color(0xFFD6D6D6)
         val failTextColor = Color(0xFFE57373)
         val backgroundColor = Color(0xFF2E2E2E)
-        val runButtonColor = Color(0xFF72C349)
+        val runButtonColor = Color(0xFF089000)
         val navBarBackgroundColor = Color(0xFF21201E)
         val syntaxRed = Color(0xFFF92672)
     }
